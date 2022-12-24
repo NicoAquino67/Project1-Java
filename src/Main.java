10f35ec5dd2392907a1378866a0be66f8511082c
NicoAquino67 <nicoanriquez23@gmail.com>
2022-12-24 13:53:20 +0100

    ejercicio1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        threeParams(3,2, 1);
+        Coche miCoche = new Coche();
+        miCoche.masPuertas();
+        System.out.println(miCoche.puertas);
+    }
+    public static void threeParams(int a, int b, int c){
+        System.out.println("a is equal to: " + a);
+        System.out.println("b is equal to: " + b);
+        System.out.println("c is equal to: " + c);
+    }
+}
+class Coche{
+    public int puertas = 0;
+    public void masPuertas(){
+        this.puertas++;
     }
 }
