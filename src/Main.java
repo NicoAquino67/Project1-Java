cb2fe6635eba64006c40b3df5fc79acd031d9c8c
NicoAquino67 <nicoanriquez23@gmail.com>
2022-12-24 15:05:20 +0100

    ejercicio2

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,45 @@
-
 public class Main {
     public static void main(String[] args) {
-        threeParams(3,2, 1);
-        Coche miCoche = new Coche();
-        miCoche.masPuertas();
-        System.out.println(miCoche.puertas);
-    }
-    public static void threeParams(int a, int b, int c){
-        System.out.println("a is equal to: " + a);
-        System.out.println("b is equal to: " + b);
-        System.out.println("c is equal to: " + c);
-    }
-}
-class Coche{
-    public int puertas = 0;
-    public void masPuertas(){
-        this.puertas++;
+        int numeroif = 0;
+
+        if (numeroif > 0){
+            System.out.println("numero if es positivo");
+        } else if (numeroif < 0) {
+            System.out.println("numero if es negativo");
+        } else {
+            System.out.println("numero if es 0");
+        }
+
+        int numeroWhile = 0;
+        while (numeroWhile < 3){
+            System.out.println(numeroWhile + " numero While");
+            numeroWhile++;
+        }
+
+        do {
+            System.out.println(numeroWhile + " numero DoWhile");
+            numeroWhile++;
+        }while(numeroWhile < 3);
+
+        for(int numeroFor = 0; numeroFor <= 3; numeroFor++){
+            System.out.println(numeroFor + " numero For");
+        }
+        String estacion = "invierno";
+        switch (estacion){
+            case "verano":
+                System.out.println("Es Verano");
+                break;
+            case "otoño":
+                System.out.println("Es Otoño");
+                break;
+            case "invierno":
+                System.out.println("Es Invierno");
+                break;
+            case "primavera":
+                System.out.println("Es Primavera");
+                break;
+            default:
+                System.out.println("El valor ingresado no es una estacion");
+        }
     }
 }
